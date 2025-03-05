9035f143f94050b35dc24eaaeab63ef16c6f2e2b
strozw <strozw@users.noreply.github.com>
2025-03-05 12:19:00 +0900

    refactor: remove unnecessary files

--- apps/api/src/db/index.ts
+++ apps/api/src/db/index.ts
@@ -1,4 +1,4 @@
 import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/libsql';
 
-export const db = drizzle(process.env.DB_FILE_NAME!);
+export const db = drizzle(process.env.DB_FILE_NAME || "file:local.db");
